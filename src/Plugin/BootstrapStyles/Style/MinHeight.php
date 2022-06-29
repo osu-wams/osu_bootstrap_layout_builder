e1b9488dc3546c5ec8c95a148652e7617cedcc20
Alex Ruef <alex.ruef@oregonstate.edu>
2022-06-29 16:14:54 -0700

    Fixed indentation

--- src/Plugin/BootstrapStyles/Style/MinHeight.php
+++ src/Plugin/BootstrapStyles/Style/MinHeight.php
@@ -1,97 +1,97 @@
 <?php
 
- namespace Drupal\osu_bootstrap_layout_builder\Plugin\BootstrapStyles\Style;
+namespace Drupal\osu_bootstrap_layout_builder\Plugin\BootstrapStyles\Style;
 
- use Drupal\bootstrap_styles\Style\StylePluginBase;
- use Drupal\Core\Form\FormStateInterface;
+use Drupal\bootstrap_styles\Style\StylePluginBase;
+use Drupal\Core\Form\FormStateInterface;
 
- /**
-  * Style MinHeight class.
-  *
-  * @package Drupal\bootstrap_layout_builder\Plugin\Style
-  *
-  * @Style(
-  *   id = "min_height",
-  *   title = @Translation("Minimum height"),
-  *   group_id = "sizing",
-  *   weight = 0
-  * )
+/**
+* Style MinHeight class.
+*
+* @package Drupal\bootstrap_layout_builder\Plugin\Style
+*
+* @Style(
+*   id = "min_height",
+*   title = @Translation("Minimum height"),
+*   group_id = "sizing",
+*   weight = 0
+* )
+*/
+class MinHeight extends StylePluginBase {
+
+  /**
+  * {@inheritDoc}
   */
- class MinHeight extends StylePluginBase {
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config();
 
-   /**
-    * {@inheritDoc}
-    */
-   public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
-     $config = $this->config();
+    $form['sizing']['min_height'] = [
+      '#type' => 'textarea',
+      '#default_value' => $config->get('min_height'),
+      '#title' => $this->t('Minimum height'),
+      '#cols' => 60,
+      '#rows' => 5,
+    ];
 
-     $form['sizing']['min_height'] = [
-       '#type' => 'textarea',
-       '#default_value' => $config->get('min_height'),
-       '#title' => $this->t('Minimum height'),
-       '#cols' => 60,
-       '#rows' => 5,
-     ];
+    return $form;
+  }
 
-     return $form;
-   }
+  /**
+  * {@inheritDoc}
+  */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    $this->config()
+      ->set('min_height', $form_state->getValue('min_height'))
+      ->save();
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
-     $this->config()
-       ->set('min_height', $form_state->getValue('min_height'))
-       ->save();
-   }
+  /**
+  * {@inheritDoc}
+  */
+  public function buildStyleFormElements(array &$form, FormStateInterface $form_state, $storage) {
+    $form['min_height'] = [
+      '#type' => 'radios',
+      '#title' => $this->t('Minimum height'),
+      '#options' => $this->getStyleOptions('min_height'),
+      '#default_value' => $storage['min_height']['class'] ?? 0,
+      '#validated' => TRUE,
+      '#attributes' => [
+        'class' => ['field-items-alignment', 'bs_input-boxes'],
+      ],
+    ];
 
-   /**
-    * {@inheritDoc}
-    */
-   public function buildStyleFormElements(array &$form, FormStateInterface $form_state, $storage) {
-     $form['min_height'] = [
-       '#type' => 'radios',
-       '#title' => $this->t('Minimum height'),
-       '#options' => $this->getStyleOptions('min_height'),
-       '#default_value' => $storage['min_height']['class'] ?? 0,
-       '#validated' => TRUE,
-       '#attributes' => [
-         'class' => ['field-items-alignment', 'bs_input-boxes'],
-       ],
-     ];
+    // Add icons to the container types.
+    foreach ($form['min_height']['#options'] as $key => $value) {
+      $form['min_height']['#options'][$key] = '<span class="input-icon ' . $key . '"></span>' . $value;
+    }
 
-     // Add icons to the container types.
-     foreach ($form['min_height']['#options'] as $key => $value) {
-       $form['min_height']['#options'][$key] = '<span class="input-icon ' . $key . '"></span>' . $value;
-     }
+    return $form;
+  }
 
-     return $form;
-   }
+  /**
+  * {@inheritDoc}
+  */
+  public function submitStyleFormElements(array $group_elements) {
+    return [
+      'min_height' => [
+        'class' => $group_elements['min_height'],
+      ],
+    ];
+  }
 
-   /**
-    * {@inheritDoc}
-    */
-   public function submitStyleFormElements(array $group_elements) {
-     return [
-       'min_height' => [
-         'class' => $group_elements['min_height'],
-       ],
-     ];
-   }
+  /**
+  * {@inheritDoc}
+  */
+  public function build(array $build, array $storage, $theme_wrapper = NULL) {
+    $classes = [];
+    if (isset($storage['min_height']['class'])) {
+      $classes[] = $storage['min_height']['class'];
+    }
 
-   /**
-    * {@inheritDoc}
-    */
-   public function build(array $build, array $storage, $theme_wrapper = NULL) {
-     $classes = [];
-     if (isset($storage['min_height']['class'])) {
-       $classes[] = $storage['min_height']['class'];
-     }
+    // Add the classes to the build.
+    $build = $this->addClassesToBuild($build, $classes);
 
-     // Add the classes to the build.
-     $build = $this->addClassesToBuild($build, $classes);
+    return $build;
+  }
 
-     return $build;
-   }
-
- }
+}
